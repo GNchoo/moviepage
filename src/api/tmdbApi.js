f48d4173305bead93ef84b92ae1d810e1e5ae046
추경남 <127163296+GNchoo@users.noreply.github.com>
2023-04-26 19:17:11 +0900

    Update tmdbApi.js
    한글추가 + 한국지역추가

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -19,31 +19,31 @@ export const tvType = {
 
 const tmdbApi = {
   getMoviesList: (type, params) => {
-    const url = "movie/" + movieType[type];
+    const url = "movie/" + movieType[type] + "?&language=ko-KR&region=KR";
     return axiosClient.get(url, params);
   },
   getTvList: (type, params) => {
-    const url = "tv/" + tvType[type];
+    const url = "tv/" + tvType[type] + "?&language=ko-KR&region=KR";
     return axiosClient.get(url, params);
   },
   getVideos: (cate, id) => {
-    const url = category[cate] + "/" + id + "/videos";
+    const url = category[cate] + "/" + id + "/videos" + "?&language=ko-KR&region=KR";
     return axiosClient.get(url, { params: {} });
   },
   search: (cate, params) => {
-    const url = "search/" + category[cate];
+    const url = "search/" + category[cate] + "?&language=ko-KR&region=KR";
     return axiosClient.get(url, params);
   },
   detail: (cate, id, params) => {
-    const url = category[cate] + "/" + id;
+    const url = category[cate] + "/" + id + "?&language=ko-KR&region=KR";
     return axiosClient.get(url, params);
   },
   credits: (cate, id) => {
-    const url = category[cate] + "/" + id + "/credits";
+    const url = category[cate] + "/" + id + "/credits" + "?&language=ko-KR&region=KR";
     return axiosClient.get(url, { params: {} });
   },
   similar: (cate, id) => {
-    const url = category[cate] + "/" + id + "/similar";
+    const url = category[cate] + "/" + id + "/similar" + "?&language=ko-KR&region=KR";
     return axiosClient.get(url, { params: {} });
   },
 };
